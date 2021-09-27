9f945822d792e4499214e2b2805b80d49a2bb2db
maddscorpian <maddscorpian@gmail.com>
2021-09-27 20:03:22 +0530

    Habit log changes

--- src/app/components/habit-log/habit-log.component.ts
+++ src/app/components/habit-log/habit-log.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-habit-log',
@@ -7,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabitLogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  name?:string;
+  count?:number;
+
+  habits?:any[]=[];
+
+
+
+  habitLog : any[] = [];
 
   ngOnInit(): void {
+    this.getHabits();
+    this.refreshList();
+  }
+
+  getHabits(){
+
+    
+      this.http.get<any>(environment.API_URL+'habit')
+      .subscribe((data: any[] | undefined)=>{
+        this.habits=data;
+      });
+    
+
+  }
+
+  createHabitLog(){
+    if (this.name === undefined){
+      alert("Habit not selected.");
+      return;
+    }
+    var val={
+      name:this.name,
+      count:this.count
+    };
+
+    this.http.post(environment.API_URL+'habit-log',val)
+    .subscribe(res=>{
+      alert(res.toString());
+      this.refreshList();
+    });
+
+  }
+
+  addHabitLog(): void {
+    
+  }
+
+
+  refreshList(){
+    this.http.get<any>(environment.API_URL+'habit-log')
+    .subscribe(data=>{
+      this.habitLog=data;
+      alert(this.habitLog[0]._id);
+      
+    });
   }
 
 }
